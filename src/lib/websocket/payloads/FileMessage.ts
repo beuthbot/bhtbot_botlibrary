60a455242ef4d0607f75cb8cd592920c6a81081d
s79912 <s79912@beuth-hochschule.de>
2021-01-27 15:47:55 +0100

    change file buffer to buffer rather than arraybuffer

--- src/lib/websocket/payloads/FileMessage.ts
+++ src/lib/websocket/payloads/FileMessage.ts
@@ -2,7 +2,7 @@ export class FileMessage {
   static type = 'file';
   userId: string;
   fileName: string;
-  binary: ArrayBuffer;
+  binary: Buffer;
 
   static isFile(obj: any) {
     return obj && obj.type === FileMessage.type && obj.fileName && obj.userId && obj.binary;
