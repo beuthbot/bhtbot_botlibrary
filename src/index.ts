8ba1723d602e133d651ecef36468b193f821df4a
s79912 <s79912@beuth-hochschule.de>
2021-02-06 23:14:43 +0100

    export more types

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ import BotRequest from './lib/botRequest';
 import BotResponse from './lib/botResponse';
 import BotUser from './lib/botUser';
 import Gateway from './lib/gateway';
+import { WebSocketConnector } from './lib/websocket/connector';
+import { FileMessage } from './lib/websocket/payloads/FileMessage';
+import { ServiceMessage } from './lib/websocket/payloads/ServiceMessage';
 
-export { Gateway, BotRequest, BotResponse, BotUser, BotIntent };
+export { Gateway, BotRequest, BotResponse, BotUser, BotIntent, WebSocketConnector, ServiceMessage, FileMessage };
